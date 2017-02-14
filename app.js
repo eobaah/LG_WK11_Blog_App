ad383eae31c83e95f31adf6525103f2741bdd49e
eobaah <eobaah@gmail.com>
2017-02-13 18:12:49 -0800

    able to display data on the front end

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -15,12 +16,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 app.use('/', index);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var path = require( 'path' )
+const { db,getAllPosts, getPost, createPost, editPost, deletePost } = require( '../database/database')
+const moment = require ( 'moment' )
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', posts: [] });
 });
+
+router.get('/test', (req, res) => {
+  getAllPosts().then( posts => {
+    res.render('index', { posts } )
+  })
+})
 
 module.exports = router;
